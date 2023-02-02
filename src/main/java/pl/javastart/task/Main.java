8af7ed9a90646a0c62bbb594d99df6141ed2e57b
Mateusz Gałecki <m.galecki10@gmail.com>
2023-02-02 23:56:03 +0100

    v0.1

--- src/main/java/pl/javastart/task/Main.java
+++ src/main/java/pl/javastart/task/Main.java
@@ -1,5 +1,11 @@
 package pl.javastart.task;
 
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.TemporalAccessor;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
@@ -10,7 +16,37 @@ public class Main {
     }
 
     public void run(Scanner scanner) {
-        // uzupełnij rozwiązanie. Korzystaj z przekazanego w parametrze scannera
+        List<String> patterns = List.of(Type.DATE_TIME_STANDARD.getPattern(),
+                Type.DATE_TIME_DOT.getPattern());
+        System.out.println("Podaj datę i czas:");
+        String dateInput = scanner.nextLine();
+        for (String pattern : patterns) {
+            try {
+                DateTimeFormatter dateTimePattern = DateTimeFormatter.ofPattern(pattern);
+                TemporalAccessor temporalAccessor = dateTimePattern.parse(dateInput);
+                LocalDateTime rightPattern = LocalDateTime.from(temporalAccessor);
+                showTime(rightPattern, dateTimePattern);
+            } catch (DateTimeException e) {
+                //ignore
+            }
+        }
     }
 
+    private void showTime(LocalDateTime time, DateTimeFormatter pattern) {
+        System.out.println("Czas lokalny: " + time.atZone(ZoneId.systemDefault()).format(pattern));
+        timeZone(pattern);
+    }
+
+    private void timeZone(DateTimeFormatter pattern) {
+        Map<String, String> zones = new LinkedHashMap<>();
+        zones.put("UTC", "UTC");
+        zones.put("London", "Europe/London");
+        zones.put("Los Angeles", "America/Los_Angeles");
+        zones.put("Sydney", "Australia/Sydney");
+        for (String key : zones.keySet()) {
+            System.out.println(key + ": " + ZonedDateTime.of(LocalDateTime.now(ZoneId.of(zones.get(key))),
+                    ZoneId.of(zones.get(key))).format(pattern));
+        }
+    }
 }
+
